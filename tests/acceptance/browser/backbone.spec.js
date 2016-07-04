14e4f96f33d65ed987815e083420057bb104e402
Sergio Álvarez <sergio.alvarez@mediasq.com>
2016-07-04 22:54:40 +0200

    Add acceptance tests for the browser

---
+++ tests/acceptance/browser/backbone.spec.js
@@ -0,0 +1,40 @@
+(function(Backbone, _, Factotum) {
+  const Job = Backbone.Model.extend({});
+
+  const JobCollection = Backbone.Collection.extend({ model: Job });
+
+  const User = Backbone.Model.extend({
+    parse(attrs) {
+      this.jobs = new JobCollection(attrs.jobs);
+
+      return _.omit(attrs, 'jobs');
+    }
+  });
+
+  describe('Acceptance', function() {
+    describe('working with Backbone', function() {
+      beforeEach(function() {
+        Factotum.define('job', Job, { name: Factotum.sequence((i) => `Job ${i}`) });
+
+        Factotum.define('user', User, {
+          id: Factotum.sequence((i) => i),
+          name: Factotum.sequence((i) => `User ${i}`),
+          jobs: Factotum.create('job', 5)
+        });
+      });
+
+      it('creates a user model with a collection of jobs', function() {
+        const user = Factotum.create('user');
+
+        user.get('jobs').should.exist;
+      });
+
+      it('accepts extra options for creating the required objects', function() {
+        const user = Factotum.create('user', { parse: true });
+
+        user.jobs.should.be.an.instanceof(JobCollection);
+      });
+    });
+  });
+
+})(window.Backbone, window._, window.Backbone.Factotum);
